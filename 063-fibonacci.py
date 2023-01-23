e2302017ca402d7216b94243a2e3929b9f4e6780
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-01-23 08:44:47 -0300

    Create 063-fibonacci.py

---
+++ 063-fibonacci.py
@@ -0,0 +1,22 @@
+print('\033[1mChallenge: generate a program that displays n elements of the Fibonacci Sequencee\033[m')
+print('---'*28)
+print('')
+
+n = int(input('Enter up to which element number you want to see: '))
+calculation = 0
+print('')
+
+t1 = 0
+t2 = 1
+
+print(f'{t1} → {t2}', end='')
+count = 3
+
+while count <= n:
+    t3 = t1 + t2
+    print(f' → {t3}', end='')
+    count += 1
+    t1 = t2
+    t2 = t3
+
+print(' →→ END')
