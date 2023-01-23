bfa8188a878be3bb93050b858efde2fc60448f62
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-01-23 10:26:25 -0300

    commit 064

---
+++ parte-2-estruturas-de-controle/064-numeros-e-condicao-de-parada.py
@@ -0,0 +1,17 @@
+print('Enter as many numbers as you want and know the total sum between them')
+print('----'*20)
+print('*If you want to stop the sum, type 999')
+print('')
+
+n = 0
+count = 0
+add = 0
+
+while n != 999:
+    n = float(input('Type the number: '))
+    add += n
+    count += 1
+
+print('')
+print(f'You entered {count-1} numbers. Adding them all up, the total is {add-999}')
+print('FIM')
