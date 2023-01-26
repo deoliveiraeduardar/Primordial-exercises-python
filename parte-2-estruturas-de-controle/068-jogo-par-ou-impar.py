cece2d200685ab6d92b5781f08a5b8efd858c159
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-01-26 15:29:32 -0300

    Update 068-jogo-par-ou-impar.py

--- parte-2-estruturas-de-controle/068-jogo-par-ou-impar.py
+++ parte-2-estruturas-de-controle/068-jogo-par-ou-impar.py
@@ -18,10 +18,17 @@ while True:
     for d in range(1, 10 + 1):
         lista.append(d)
     maquina_numero = random.choice(lista)
-    humano_numero = int(input('\033[1mUm, dois, três e já! Digite um número de 0 a 10: \033[m'))
 
     while par_ou_impar not in 'PI':
         par_ou_impar = str(input('\033[1mLevando em conta P para par e I para ímpar. Digite a sua escolha: \033[m')).upper()
+
+    humano_numero = int(input('\033[1mUm, dois, três e já! Digite um número de 0 a 10: \033[m'))
+    if humano_numero < 0 or humano_numero > 10:
+        while True:
+            print('')
+            print('Você não digitou um número no intervalo que combinamos. Tente de novo!')
+            time.sleep(1)
+            humano_numero = int(input('\033[1mUm, dois, três e já! Digite um número de 0 a 10: \033[m'))
 
     soma = maquina_numero + humano_numero
     print('')
@@ -36,6 +43,7 @@ while True:
         print(f'A soma de {maquina_numero}+{humano_numero}= {soma}. É par!')
         print('')
         time.sleep(0.5)
+        contador += 1
         if par_ou_impar in 'P':
             print('Como você escolheu par, você ganhou! Parabéns, humano!')
             time.sleep(1)
@@ -43,14 +51,12 @@ while True:
             time.sleep(1)
             print('Por favor, deixa eu treinar o meu algoritmo com você')
             print(emoji.emojize('Vamos lá de novo!:pleading_face:'))
-            contador += 1
             time.sleep(3)
 
             print('')
         else:
             print('Como você escolheu ímpar, você perdeu!!')
             print(emoji.emojize('Nós, máquinas, vamos dominar o mundo! :smiling_face_with_sunglasses:'))
-            contador += 1
             break
 
     # SE A SOMA FOR ÍMPAR
@@ -58,6 +64,7 @@ while True:
         print(f'A soma de {maquina_numero}+{humano_numero}= {soma}. É ímpar!')
         print('')
         time.sleep(0.5)
+        contador += 1
         if par_ou_impar in 'I':
             print('Como você escolheu ímpar, você ganhou! Parabéns, humano!')
             time.sleep(1)
@@ -65,11 +72,10 @@ while True:
             time.sleep(1)
             print('Por favor, deixa eu treinar o meu algoritmo com você')
             print(emoji.emojize('Vamos lá de novo!:pleading_face:'))
-            contador += 1
             time.sleep(3)
         else:
             print('Como você escolheu par, você perdeu!!')
             print(emoji.emojize('Nós, máquinas, vamos dominar o mundo!:smiling_face_with_sunglasses:'))
-            contador += 1
             break
-print(f'Nos jogamos {contador} vezes! Foi divertido. Até mais!')
+# VERIFICAR O CONTADOR
+print(f'Eu ganhei {contador} vezes! Foi divertido. Até mais!')
