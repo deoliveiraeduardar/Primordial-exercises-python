4c6c902aecee429f0d96752539aafa3701012a92
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-07 18:03:22 -0300

    Update 074-sorteio.py

--- parte-3-estruturas-compostas/074-sorteio.py
+++ parte-3-estruturas-compostas/074-sorteio.py
@@ -3,11 +3,18 @@ from random import randint
 lista = []
 
 quantidade = int(input('Digite quantos números aleatórios você quer gerar: '))
+print('')
+
 for d in range(0, quantidade):
-    elemento = randint(0,100)
+    elemento = randint(0, 100)
     lista.append(elemento)
 
 # Converter lista em tulpa
-print(f'Lista:', lista)
-tudo_na_tulpa = tuple(lista)
-print(f'Cinco números aleatórios gerados:: ', tudo_na_tulpa)
+tulpa = tuple(lista)
+print(f'Cinco números aleatórios gerados:: ', tulpa)
+print(f'Números em ordem alfabética: {sorted(tulpa)}')
+print('')
+
+print(f'Menor nº na tupla: {min(tulpa)}')
+print(f'Maior nº na tupla: {max(tulpa)}')
+
