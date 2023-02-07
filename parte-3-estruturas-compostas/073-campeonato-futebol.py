46ea175991ea8bb4e148ad5e6ed1bc526635b61b
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-07 17:34:13 -0300

    Update 073-campeonato-futebol.py

--- parte-3-estruturas-compostas/073-campeonato-futebol.py
+++ parte-3-estruturas-compostas/073-campeonato-futebol.py
@@ -1,4 +1,4 @@
-times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Atlético Paranaense', 'Atlético Mineiro', 'Fortaleza', 'São Paulo', 'América FC', 'Botafogo', 'Santos', 'Goiás', 'Red Bull', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético', 'Avaí', 'Juventude')
+times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Athletico Paranaense', 'Atlético Mineiro', 'Fortaleza', 'São Paulo', 'América FC', 'Botafogo', 'Santos', 'Goiás', 'Red Bull', 'Coritiba', 'Cuiabá', 'Ceará', 'Atlético', 'Avaí', 'Juventude')
 
 print(f'Cinco primeiros colocados: {times[0:6]}')
 print(f'Quatro últimos colocados: {times[16:20]}')
