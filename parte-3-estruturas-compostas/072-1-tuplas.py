ce2358b9c782515ff95ba051f045a261fdd01421
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-07 08:29:54 -0300

    Update 072-1-tuplas.py

--- parte-3-estruturas-compostas/072-1-tuplas.py
+++ parte-3-estruturas-compostas/072-1-tuplas.py
@@ -1,12 +1,27 @@
 print('====' * 5, 'TUPLAS', '====' * 5)
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
 
+print('\033[1mFazendo com   for cont in range(0, len(lanche):\033[m')
 for cont in range(0, len(lanche)):
     print(f'Eu vou comer {lanche[cont]}')
 
-
+print('')
+print('\033[1mFazendo com   for comida in lanche:\033[m')
 for comida in lanche:
     print(f'Eu vou comer {comida}')
 
-print('Total de índices de lanche:', len(lanche))
+print('')
+
+print('\033[1mFazendo pra gerar a posição, com   for cont in range(0, len(lanche)):\033[m')
+print('*não tem como gerar a posição usando, diretamente, in')
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+print('')
+print('\033[1mFazendo pra gerar a posição, com   for pos, comida in enumerate(lanche):\033[m')
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer comida {comida} na posição {pos}')
+
+print()
+print('  - Total de índices de lanche:', len(lanche))
 print('  - Comi na proporção correta')
