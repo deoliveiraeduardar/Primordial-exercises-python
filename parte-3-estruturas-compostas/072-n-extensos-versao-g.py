6b13eb29b16009080b7ab1bb76f0a59d3f5b1fc7
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-07 10:27:03 -0300

    Create 072-n-extensos-versao-g.py

---
+++ parte-3-estruturas-compostas/072-n-extensos-versao-g.py
@@ -0,0 +1,22 @@
+from num2words import num2words
+
+# CRIAÇÃO DE LISTA POR EXTENSO A PARTIR DO FOR
+lista = []
+
+for d in range(0, 20+1):
+    n_extenso = num2words(d, lang='pt-br').title()
+    lista.append(n_extenso)
+print('')
+
+# CONVERSÃO DA LISTA PARA TULPA
+# print(f'Lista:', lista)
+tulpa = tuple(lista)
+# print(f'Tulpa: ', tulpa)
+
+# WHILE
+while True:
+    n_usuario = int(input('Digite um número de 0 a 20: '))
+    if 0 <= n_usuario <= 20 :
+        break
+    print('Tente novamente. ', end='')
+print('O número por extenso é: ', tulpa[n_usuario])
