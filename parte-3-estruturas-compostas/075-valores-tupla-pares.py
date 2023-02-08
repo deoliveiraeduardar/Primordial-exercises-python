0ce5676bb19d3300215c5bee6a8b3b4b25343aea
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-08 07:26:17 -0300

    Update 075-valores-tupla-pares.py

--- parte-3-estruturas-compostas/075-valores-tupla-pares.py
+++ parte-3-estruturas-compostas/075-valores-tupla-pares.py
@@ -1,15 +1,22 @@
 
-n1 = float(input('Digite um número: '))
-n2 = float(input('Digite mais um número: '))
-n3 = float(input('Digite outro número: '))
-n4 = float(input('Digite o último número: '))
+tupla = (int(input('Digite um número: ')),
+     int(input('Digite mais número: ')),
+     int(input('Digite outro número: ')),
+     int(input('Digite o último número: ')))
 
-tupla = (n1, n2, n3, n4)
 print(tupla)
 print('')
 
-print(f'Vezes que apareceram o nº 9: {tupla.count(9)}')
-print(f'Em qual posição foi digitado o primeiro 3: {tupla.index(3)}')
+if 9 in tupla:
+    print(f'Vezes que apareceram o nº 9: {tupla.count(9)}')
+else:
+    print('O número 9 não apareceu nenhuma vez.')
 
-par = n/ 2
-if n % ==
+if 3 in tupla:
+    print(f'Vezes que apareceram o nº 9: {tupla.index(3) + 1}')
+else:
+    print('O número 3 não apareceu nenhuma vez.')
+
+for numero in tupla:
+    if numero % 2 == 0:
+        
