23fde28663d106a3714b1beaea25f9058380727e
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-07 21:39:28 -0300

    Update 075-valores-tupla-pares.py

--- parte-3-estruturas-compostas/075-valores-tupla-pares.py
+++ parte-3-estruturas-compostas/075-valores-tupla-pares.py
@@ -6,7 +6,10 @@ n4 = float(input('Digite o último número: '))
 
 tupla = (n1, n2, n3, n4)
 print(tupla)
+print('')
 
 print(f'Vezes que apareceram o nº 9: {tupla.count(9)}')
 print(f'Em qual posição foi digitado o primeiro 3: {tupla.index(3)}')
 
+par = n/ 2
+if n % ==
