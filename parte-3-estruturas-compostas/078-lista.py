485040d9add8df86f110f78e04f1ec70794d0467
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-09 09:19:56 -0300

    Create 078-lista.py

---
+++ parte-3-estruturas-compostas/078-lista.py
@@ -0,0 +1,10 @@
+lista = ['sqr', 'node', 'pyscript', 'json', 'mysql', 'python']
+if 'python' in lista:
+    lista.remove('python')
+print(lista)
+
+#CRIANDO UMA LISTA COM A FUNÇÃO LITA E COM RANGE
+valores = list(range(4,11,2))
+print(valores)
+valores.sort()
+print(valores)
