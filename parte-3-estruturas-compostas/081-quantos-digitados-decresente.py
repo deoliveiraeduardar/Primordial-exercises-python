eaeefaf2d8a3a2b8ab93706bfd0b54025ac0d6f3
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-10 15:02:44 -0300

    Update 081-quantos-digitados-decresente.py

--- parte-3-estruturas-compostas/081-quantos-digitados-decresente.py
+++ parte-3-estruturas-compostas/081-quantos-digitados-decresente.py
@@ -7,20 +7,21 @@ while True:
 
     print('   Caso queira continuar digitando, digite S para sim e N para não.')
     escolha = str(input('\033[1mDigite a sua opção: \033[m')).upper()
-
+    print('')
     while escolha not in 'SN':
         print('   Dados inválidos.')
         print('')
-
         escolha = str(input('\033[1mDigite novamente: \033[m')).upper()
+
     if escolha in 'N':
         print('Entrou no break')
         quantidade = len(lista)
-        print(f'Lista decrescente: {lista.sort(reverse=True)}')
-
-        break
-
 
 print('')
-print('PASSOU DO WHILE TRUE.')
-print(f'Quantidade de elementos: {quantidade}')
+if 5 in lista:
+    print(f'-  O número 5 está na lista.')
+elif 5 not in lista:
+    print(f'-  O número 5 não está na lista.')
+print(f'-  Quantidade de elementos: {quantidade}')
+lista.sort(reverse=True)
+print(f'-  Lista decrescente: {lista}')
