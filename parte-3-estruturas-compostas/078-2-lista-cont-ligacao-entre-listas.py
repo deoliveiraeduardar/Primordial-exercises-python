9a5f099b169dbae7c09000f9ef89ae3f910f810d
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-10 09:24:16 -0300

    create 078

--- parte-3-estruturas-compostas/078-2-lista-cont-ligacao-entre-listas.py
+++ parte-3-estruturas-compostas/078-2-lista-cont-ligacao-entre-listas.py
@@ -8,3 +8,4 @@ b[2] = 8
 print(f'Colocando B na posição 2 (b[2] = 8) fica: ')
 print(f'Lista A: {a}')
 print(f'Lista B: {b}')
+print('')
---
+++ parte-3-estruturas-compostas/078-2-lista-copia-nao-ligacao.py
@@ -0,0 +1,8 @@
+a = [2, 3, 4, 7]
+b = a[:]
+b[2] = 8
+
+print(f'Colocando (b = a[:]) cria uma cópia de A dentro de B')
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+print('')
