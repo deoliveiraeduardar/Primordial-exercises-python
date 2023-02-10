85912b3f28698b2e07c67ba6e75831b4e928e2e3
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-10 15:33:30 -0300

    Update 083-lista-parenteses.py

---
+++ parte-3-estruturas-compostas/083-lista-parenteses.py
@@ -0,0 +1,16 @@
+print('Vamos verificar se a sua expressão matemática está com a quantidade certa de parênteses!')
+print('=====' * 18)
+print('')
+
+frase = str(input('Digite uma expressão matemática com parênteses: '))
+list(frase)
+print(frase)
+print(len(frase))
+print('')
+
+frase.split()
+print(frase)
+
+# USAR LEN
+# LOACALIZAR O LOCAL ONDE TEM O PRIMEIRO PARÊNTESES
+#D
