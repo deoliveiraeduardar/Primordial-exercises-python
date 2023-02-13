862b823b6e8c992925dfacd85757390a6daaae92
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-13 09:00:29 -0300

    commit 080 082

--- parte-3-estruturas-compostas/080-valores-lista-sem-sort.py
+++ parte-3-estruturas-compostas/080-valores-lista-sem-sort.py
@@ -1,14 +1,23 @@
-lista = []
+lista = list()
 
-for d in range(1, 5 + 1):
-    numeros = int(input(f' Digite o {d}º número: '))
+for c in range(1, 5 + 1):
+    numeros = int(input(f' Digite o {c}º número: '))
     print(f'LISTA: {lista}')
 
+
     # Ou lista[-1]
-    if d == 0 or numeros > lista[len(lista)-1]:
+    if c == 0:
+        lista.append(numeros)
+
+    if numeros > lista[-1]:
         lista.append(numeros)
 
     else:
         pos = 0
         while pos < len(lista):
-            
+            if numeros <= lista[pos]:
+                lista.insert(pos, numeros)
+                break
+            pos += 1
+    print('===' * 30)
+    print(f'Os valorse igitados em ordem crescente sem sort foram: {lista}')
