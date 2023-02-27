8826c38f32c15e24f8e324532928b07803aa95f3
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 13:07:39 -0300

    create

---
+++ parte-3-estruturas-compostas/084-0-lista-composta.py
@@ -0,0 +1,7 @@
+# Lista pessoas é composta por 0, 1 e 2 índices
+pesoas = [['Pedro', 25], ['Maria', 19], ['João', 32]]
+print(pessoas)
+
+print(f'Print no índice 0 e item 0 (pessoas[0][0]): {pessoas[0][0]})')
+print(f'Print no índice 1 e item 1 (pessoas[1][1]): {pessoas[1][1]})')
+print(f'Print no índice 1 e item 0 (pessoas[1][0]): {pessoas[1][0]})')
