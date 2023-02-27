dfd9dff5d902b3cbe6d90f15c495ea8d61963786
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 11:21:18 -0300

    Update 083-lista-parenteses.py

--- parte-3-estruturas-compostas/083-lista-parenteses.py
+++ parte-3-estruturas-compostas/083-lista-parenteses.py
@@ -1,26 +1,26 @@
 print('Vamos verificar se a sua expressão matemática está com a quantidade e ordem certa de parênteses.')
 print('=====' * 18)
 print('')
-# Solução: 
+# Solução:
 
 expr = str(input('Digite a expressão matemática com parênteses: '))
-lista_verificador = []
+lista_verificadora = []
 
 for simbolo in expr:
     if simbolo == '(':
-        lista_verificador.append('(')
-        # Quando tiver parêntese aberto, adicionar na lista_verificadorr.
+        lista_verificadora.append('(')
+        # Quando tiver parêntese aberto, adicionar ( na lista_verificadorr.
 
     elif simbolo == ')':
-        if len(lista_verificador) > 0:
-            lista_verificador.pop()
+        if len(lista_verificadora) > 0:
+            lista_verificadora.pop()
         else:
-            lista_verificador.append(')')
+            lista_verificadora.append(')')
             break
         # Quando tiver parêntese fechado, remover da lista_verificadorr.
 
 
-if len(lista_verificador) == 0:
+if len(lista_verificadora) == 0:
     print('Sua epressão é válida!')
 else:
     print('Sua expressão é inválida!')
