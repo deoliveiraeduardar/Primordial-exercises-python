8c8d4d6664669d2e594357b73e39d1e437f60396
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 11:15:35 -0300

    Update 083-lista-parenteses.py

--- parte-3-estruturas-compostas/083-lista-parenteses.py
+++ parte-3-estruturas-compostas/083-lista-parenteses.py
@@ -13,20 +13,5 @@ for simbolo in expr:
         if len(pilha) > 0:
             pilha.pop()
         else:
-            pilha.app(')')
+            pilha.append(')')
             break
-
-
-list(expr)
-print(f'Essa é a frase: {expr}')
-print(f'Quantidade de caracteres: {len(expr)}')
-print('')
-
-print(frase)
-
-
-frase.index('(')
-
-# USAR LEN
-# LOACALIZAR O LOCAL ONDE TEM O PRIMEIRO PARÊNTESES
-#D
