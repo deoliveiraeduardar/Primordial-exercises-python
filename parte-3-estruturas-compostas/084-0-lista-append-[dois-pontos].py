f39c399c5ab14d723a28100dc9c97f93c49a7426
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 13:58:33 -0300

    Create 084-0-lista-append-[dois-pontos].py

---
+++ parte-3-estruturas-compostas/084-0-lista-append-[dois-pontos].py
@@ -0,0 +1,24 @@
+lista_teste = list()
+lista_teste.append('Gustavo')
+lista_teste.append(40)
+print(f'Print da lista teste: {lista_teste}')
+print('')
+
+lista_galera = list()
+print('')
+
+lista_galera.append(lista_teste[:])
+print('Após galera.append(lista_teste):')
+print(f'Print da lista_galera: {lista_galera}')
+print('')
+
+print('Agora vou mudar a lista_teste[0] = Eduarda e lista_teste[1] = 26:')
+lista_teste[0] = 'Eduarda'
+lista_teste[1] = 26
+lista_galera.append(lista_teste[:])
+print('')
+
+# A resposta fica diferente, pois usei [:].
+# Dessa forma, a lista_teste original fica com Gustavo.
+# E a lista_galera fica com Eduarda.
+print(f'Print da lista_galera: {lista_galera}')
