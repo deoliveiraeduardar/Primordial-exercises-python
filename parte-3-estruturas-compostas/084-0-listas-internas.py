6197f0b783980ef80843d645ea93697b7791fcc6
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 14:10:37 -0300

    Update 084-0-listas-internas.py

--- parte-3-estruturas-compostas/084-0-listas-internas.py
+++ parte-3-estruturas-compostas/084-0-listas-internas.py
@@ -1,7 +1,8 @@
 galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Eduarda', 26]]
 
 for item in galera:
-    print(f'Print do item total: {item}')
+    print(f'Print do índice: {item}')
     print(f'Print do item 0 (nome) do item: {item[0]}')
     print(f'Print o item 1 (idade) do item: {item[1]}')
+    print(f'{item[0]} tem {item[1]} anos de idade.')
     print('')
