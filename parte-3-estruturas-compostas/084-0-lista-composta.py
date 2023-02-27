27942cd2cb7d7cf1db541fd9cabec75706e13684
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-27 13:14:26 -0300

    create 084-0

--- parte-3-estruturas-compostas/084-0-lista-composta.py
+++ parte-3-estruturas-compostas/084-0-lista-composta.py
@@ -1,7 +1,17 @@
+dados = ['Pedro', 25]
+print(dados)
+
+pessoas = list()
+# Peguei dados, coloquei uma cópia dos dados e vou dar um append
+# Agora tenho uma lista (pessoas). E dentro dessa lista tenho outra lista (dados)
+pessoas.append(dados[:])
+print('')
+
 # Lista pessoas é composta por 0, 1 e 2 índices
-pesoas = [['Pedro', 25], ['Maria', 19], ['João', 32]]
+pessoas = [['Pedro', 25], ['Maria', 19], ['João', 32]]
 print(pessoas)
 
-print(f'Print no índice 0 e item 0 (pessoas[0][0]): {pessoas[0][0]})')
-print(f'Print no índice 1 e item 1 (pessoas[1][1]): {pessoas[1][1]})')
-print(f'Print no índice 1 e item 0 (pessoas[1][0]): {pessoas[1][0]})')
+print(f'Print no índice 0 e item 0 (pessoas[0][0]): {pessoas[0][0]}')
+print(f'Print no índice 2  (pessoas[2]): {pessoas[2]}')
+print(f'Print no índice 1 e item 1 (pessoas[1][1]): {pessoas[1][1]}')
+print(f'Print no índice 2 e item 0 (pessoas[2][0]): {pessoas[2][0]}')
--- parte-3-estruturas-compostas/084-0-lista.py
+++ parte-3-estruturas-compostas/084-0-lista.py
@@ -5,11 +5,13 @@ pessoas = list()
 # Peguei dados, coloquei uma cópia dos dados e vou dar um append
 # Agora tenho uma lista (pessoas). E dentro dessa lista tenho outra lista (dados)
 pessoas.append(dados[:])
+print('')
 
 # Lista pessoas é composta por 0, 1 e 2 índices
 pessoas = [['Pedro', 25], ['Maria', 19], ['João', 32]]
 print(pessoas)
 
-print(f'Print no índice 0 e item 0 (pessoas[0][0]): {pessoas[0][0]})')
-print(f'Print no índice 1 e item 1 (pessoas[1][1]): {pessoas[1][1]})')
-print(f'Print no índice 2 e item 0 (pessoas[2][0]): {pessoas[2][0]})')
+print(f'Print no índice 0 e item 0 (pessoas[0][0]): {pessoas[0][0]}')
+print(f'Print no índice 1 e item 1 (pessoas[1][1]): {pessoas[1][1]}')
+print(f'Print no índice 2 e item 0 (pessoas[2][0]): {pessoas[2][0]}')
+print(f'Print no índice 2  (pessoas[2]): {pessoas[2]}')
