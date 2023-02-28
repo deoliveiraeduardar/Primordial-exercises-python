f5b9765e4c75c375aba2d8a7d979eb11e679252c
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-28 14:36:59 -0300

    085

--- parte-3-estruturas-compostas/085-valores-pares-impares-crescentes.py
+++ parte-3-estruturas-compostas/085-valores-pares-impares-crescentes.py
@@ -6,13 +6,22 @@ pares = []
 impares = []
 
 while True:
-    temporario.append(float(input('Digite o valor: ')))
+    temporario.append(int(input('Digite o valor: ')))
     lista_unica.append(temporario[:])
     print(f'Printando temporário: {temporario}')
     print(f'Printando lista_unica: {lista_unica}')
-    temporario.clear()
 
-    
+    if temporario % 2 == 0:
+        print(f'{temporario} é um número par')
+        pares.append(temporario[:])
+        print(f'Printando lista de pares: {pares}')
+    else:
+        print(f'{temporario} é um número ímpar')
+        impares.append(temporario[:])
+        print(f'Printando lista de ímpares: {impares}')
+
+    temporario.clear()
+    print('Chegamos após temporario clear')
     print(f'Printando lista_unica máximo : {max(lista_unica)}')
     print(f'Printando lista_unica minimo : {min(lista_unica)}')
 
