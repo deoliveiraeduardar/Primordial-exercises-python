255279bda3506330a80f05c07df39b5f4a92fc55
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-28 15:47:14 -0300

    Update 096-teste.py

--- parte-3-estruturas-compostas/096-teste.py
+++ parte-3-estruturas-compostas/096-teste.py
@@ -9,3 +9,8 @@ for d in range(1, 8):
     else:
         num[1].append(valor)
         print(f'Printando lista num APÓS INSERIR ÍMPAR: {num}')
+
+num[0].sort()
+num[1].sort()
+print(f'Valores pares em ordem: {num[0]}')
+print(f'Valores ímpares em ordm: {num[1]}')
