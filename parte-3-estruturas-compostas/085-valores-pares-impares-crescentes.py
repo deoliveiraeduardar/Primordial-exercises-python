180f71a3083d3aead149c846818d73fdbd1f7dbd
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-02-28 14:27:25 -0300

    create 085

--- parte-3-estruturas-compostas/085-valores-pares-impares-crescentes.py
+++ parte-3-estruturas-compostas/085-valores-pares-impares-crescentes.py
@@ -9,5 +9,11 @@ while True:
     temporario.append(float(input('Digite o valor: ')))
     lista_unica.append(temporario[:])
     print(f'Printando temporário: {temporario}')
+    print(f'Printando lista_unica: {lista_unica}')
     temporario.clear()
+
+    
+    print(f'Printando lista_unica máximo : {max(lista_unica)}')
+    print(f'Printando lista_unica minimo : {min(lista_unica)}')
+
     print('')
