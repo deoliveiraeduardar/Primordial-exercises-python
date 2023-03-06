00bb15a22f4ebfd6797a19d0d815ed302598fdbd
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-03-06 17:03:37 -0300

    Update 088-mega-sena.py

--- parte-3-estruturas-compostas/088-mega-sena.py
+++ parte-3-estruturas-compostas/088-mega-sena.py
@@ -1,8 +1,14 @@
+from random import randint
+cont = 0
+lista = list()
+while True:
+    num = randint(1, 60)
+    if num in lista:
+
 # Quantos jogos vai gerar
 # Cada jogo tem 6 numeros diferentes em cada jogo
 # Numeros: 1 a 60
 # Tudo cadastrado em lista composta
-jogos = []
 
 while True:
     quantidade = int(input('Digite quantos jogos você quer fazer: '))
