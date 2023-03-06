f9ddd8446389df70c45c1e430bcf8a180d696f74
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-03-06 17:04:58 -0300

    Update 088-mega-sena.py

--- parte-3-estruturas-compostas/088-mega-sena.py
+++ parte-3-estruturas-compostas/088-mega-sena.py
@@ -3,7 +3,13 @@ cont = 0
 lista = list()
 while True:
     num = randint(1, 60)
-    if num in lista:
+    if num not in lista:
+        lista.append(num)
+        cont += 1
+    if cont >= 6:
+        print('Entrou no break')
+        break
+
 
 # Quantos jogos vai gerar
 # Cada jogo tem 6 numeros diferentes em cada jogo
