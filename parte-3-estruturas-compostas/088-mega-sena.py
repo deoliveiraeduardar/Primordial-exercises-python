d129a6ad8e1709ad27f3c7d841c2ffb73a597a99
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-03-06 17:05:16 -0300

    Update 088-mega-sena.py

--- parte-3-estruturas-compostas/088-mega-sena.py
+++ parte-3-estruturas-compostas/088-mega-sena.py
@@ -15,8 +15,3 @@ while True:
 # Cada jogo tem 6 numeros diferentes em cada jogo
 # Numeros: 1 a 60
 # Tudo cadastrado em lista composta
-
-while True:
-    quantidade = int(input('Digite quantos jogos você quer fazer: '))
-    jogos2 = [], * quantidade
-    print(jogos2)
