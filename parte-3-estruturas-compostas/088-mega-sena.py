d1a08dcf1da4fea12d0ac4945606836a1497d27f
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-03-06 17:09:48 -0300

    Update 088-mega-sena.py

--- parte-3-estruturas-compostas/088-mega-sena.py
+++ parte-3-estruturas-compostas/088-mega-sena.py
@@ -7,14 +7,14 @@ quant = int(input('Digite jogos você quer que eu sorteie: '))
 tot = 0
 while tot <= quantidade:
     cont = 0
-while True:
-    num = randint(1, 60)
-    if num not in lista:
-        lista.append(num)
-        cont += 1
-    if cont >= 6:
-        print('Entrou no break')
-        break
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            print('Entrou no break')
+            break
 lista.sort()
 print(f'Os números sorteados foram {lista}')
 
